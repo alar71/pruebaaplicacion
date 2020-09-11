d23c6720eb224e72835fd4682b1fdf1feb893c94
alar71 <71129664+alar71@users.noreply.github.com>
2020-09-11 12:56:54 +0200

    Add files via upload

---
+++ video.js
@@ -0,0 +1,6 @@
+if (screen.width >= 900) 
+var space = $(window).height();
+var diff = space - HEIGHT;
+var margin = (diff > 0) ? (space - HEIGHT)/2 : 0;
+$('#video').css({'height': espace});
+alert("tamaño de espace: "+ espace);
